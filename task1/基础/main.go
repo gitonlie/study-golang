8db680283b59bde7d730786b5718a2daa670f666
du.k <1181249332@qq.com>
2025-07-23 21:19:26 +0800

    修改

--- task1/基础/main.go
+++ task1/基础/main.go
@@ -4,32 +4,16 @@ import "fmt"
 
 // 两数之和
 func twoSum(nums []int, target int) []int {
-	tmpMap := make(map[int][]int, len(nums))
-	for i, e := range nums {
-		v, exist := tmpMap[e]
-		if exist {
-			tmpMap[e] = append([]int{i}, v...)
-		} else {
-			tmpMap[e] = []int{i}
-		}
-	}
-	fmt.Println(tmpMap)
+	vMap := map[int]int{}
 
 	var res []int
 	for i := range nums {
 		t := target - nums[i]
-		v, exist := tmpMap[t]
+		v, exist := vMap[t]
 		if exist {
-			if t == nums[i] && len(v) == 1 {
-				//表明是唯一值
-				continue
-			}
-
-			if t != nums[i] {
-				res = append(res, i)
-			}
-			res = append(res, v...)
-			break
+			res = []int{v, i}
+		} else {
+			vMap[nums[i]] = i
 		}
 	}
 
